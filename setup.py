6a7370695507de1c1473edf7450bca9d7018a457
Gian Glen <85188124+netgian@users.noreply.github.com>
2025-02-23 23:17:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pumpfun',
-    version='0.0.1',
+    version='0.3',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
     license='MIT',
